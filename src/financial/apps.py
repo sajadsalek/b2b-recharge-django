af0e177b09bfa2850581845a6529ef07d5388d28
sajad <saajadsalek@gmail.com>
2025-12-15 18:54:49 +0330

    change primary keyes

--- src/financial/apps.py
+++ src/financial/apps.py
@@ -2,5 +2,4 @@ from django.apps import AppConfig
 
 
 class FinancialConfig(AppConfig):
-    default_auto_field = "django.db.model.BigAutoField"
     name = 'src.financial'
--- src/financial/model/transaction.py
+++ src/financial/model/transaction.py
@@ -3,7 +3,6 @@ from src.common.models import BaseModel
 
 
 class Transaction(BaseModel):
-
     amount = models.DecimalField(max_digits=10, decimal_places=2)
     TransactionDate = models.DateTimeField(auto_now=True)
     description = models.CharField(max_length=255, null=True, blank=True)
